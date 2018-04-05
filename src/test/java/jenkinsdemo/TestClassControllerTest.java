d1ed161b182ade3a284f0eb0cdb345798866f2b1
Abilene <abilene.franquez@gmail.com>
2018-04-04 23:19:41 -0500

    add testing

---
+++ src/test/java/jenkinsdemo/TestClassControllerTest.java
@@ -0,0 +1,15 @@
+package jenkinsdemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestClassControllerTest {
+
+	@Test
+	public void test() {
+		assertEquals("ABC","ABC");
+		assertEquals("AB","ABC");
+	}
+
+}
